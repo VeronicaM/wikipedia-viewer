23d79d38e41ae35cdc894d15fc499eb18142b174
Veronica Mihai <Veronica Mihai>
2016-12-30 18:21:44 +0200

    added wiki search, wiki autocomplete, wiki random article

---
+++ functions.php
@@ -0,0 +1,21 @@
+  <?php
+      function getWikiInfo(){
+      
+           $wikiInfo = file_get_contents("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=".urlencode($_GET['search']));
+               
+           $response = json_decode($wikiInfo);
+           return $response;
+    }
+    function getAutocomplete(){
+            $response = file_get_contents("http://en.wikipedia.org/w/api.php?action=opensearch&datatype=jsonp&format=json&search=".$_GET['q']);
+            $response = json_decode($response);
+           return $response;
+    }
+   
+    if(isset($_GET['autocomplete'])){
+      echo json_encode(getAutocomplete()[1]);
+    }
+    else {
+      echo json_encode(getWikiInfo());  
+     } 
+?>
---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+$(function () 
+ {
+          
+	 $(".searchbox").autocomplete({
+		source: function (request, response) {
+		 jQuery.getJSON(
+			"./functions.php?autocomplete=true&q="+request.term,
+			function (data) {
+			   response(data);
+			}
+		 );
+		},
+		minLength: 3,
+		select: function (event, ui) {
+		   getWikiInfo(ui.item.value);	
+		   return false;
+		}
+	 });
+
+	$(".searchbox").autocomplete("option", "delay", 100);
+	  
+	  
+	  $(document).ready(function(){
+		    $('.searchbox').keyup(function(e){
+			      if(e.keyCode == 13)
+				    {
+				       getWikiInfo(e.currentTarget.value);
+				    }
+			});
+			$("#randomWikis").click(function(e){
+ 				getRandomWikis();
+			});
+			$("#refresh").click(function(e){
+ 				 $("#wikiInfo").html("");
+ 				 $(".searchbox").val("");
+			});
+	  });
+	 
+	   
+
+
+	function getWikiInfo(query){
+			$.getJSON("./functions.php?search="+query, function(result){
+			      let template = $("#searchResults").html();
+			      let compiledTemplate = Handlebars.compile(template);
+			      $("#wikiInfo").html(compiledTemplate(result));
+			});
+	}
+	
+	 function getRandomWikis(){
+	 	      let template = $("#randomResults").html();
+		      let compiledTemplate = Handlebars.compile(template);
+		       let mockA = Array.apply(null, Array(30)).map(function (x, i) { return i; });
+		       let placeholder = {wikis:mockA};
+		      $("#wikiInfo").html(compiledTemplate(placeholder));
+	 }
+
+     
+    
+     
+});
